5d51be34d6ab4f026cebcf7224cb32e2496e7a6d
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2024-03-09 22:03:26 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 132 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 별 찍기 - 2
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int n = 0; n < N; n++) {
+			for (int i = 0; i < N-n-1; i++) {
+				bw.write(" ");
+			}
+			for (int i = 0; i <= n; i++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		bw.close();
+	}
+	
+}
