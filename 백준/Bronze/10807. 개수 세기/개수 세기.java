15706121befcd424c27299ff03c34640e3bf2e83
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2024-03-14 22:13:08 +0900

    [Bronze V] Title: 개수 세기, Time: 140 ms, Memory: 14496 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 개수 세기
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		int count = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int V = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			if (arr[i] == V) {
+				count++;
+			}
+		}
+		
+		bw.write(String.format("%d", count));
+		bw.close();
+	}
+	
+}
