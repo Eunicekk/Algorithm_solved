3209ae3ef66c44e1d2d1decfd9b41fa6542d1840
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2024-03-22 22:05:21 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 124 ms, Memory: 14012 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 고려대는 사랑입니다
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.write("고려대학교");
+		bw.close();
+	}
+	
+}
