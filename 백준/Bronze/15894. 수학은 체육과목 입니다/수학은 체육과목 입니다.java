ed476928a6485b11ed68753e21347e033b387444
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2024-03-23 16:16:02 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 수학은 체육과목 입니다.
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		long n = Long.parseLong(br.readLine());
+		
+		bw.write(n * 4 + "");
+		bw.close();
+ 	}
+	
+}
