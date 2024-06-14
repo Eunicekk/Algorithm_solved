f4c4bbd1ea64d3a6ca1372540c420ab6ea530d50
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2024-06-14 13:03:21 +0900

    [Silver III] Title: N과 M (2), Time: 144 ms, Memory: 15764 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -3,51 +3,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// N과 M(2)
 public class Main {
-	
+
 	static int N;
 	static int M;
-	static int[] nums;
 	static int[] select;
-	static StringBuilder sb;
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		nums = new int[N];
 		select = new int[M];
-		sb = new StringBuilder();
-		
-		for (int i = 1; i <= N; i++) {
-			nums[i - 1] = i;
-		}
 		
 		combination(0, 0);
-		System.out.println(sb);
+		System.out.print(sb);
 	}
 	
-	static void combination(int index, int sindex) {
+	static void combination(int idx, int sidx) {
 		// 기저 조건
-		if (sindex == M) {
+		if (sidx == M) {
 			for (int i = 0; i < M-1; i++) {
 				sb.append(select[i] + " ");
 			}
 			sb.append(select[M-1] + "\n");
-			
 			return;
 		}
 		
-		if (index == N) {
-			return;
-		}
+		if (idx == N) return;
 		
-		
-		// 재귀 부분
-		select[sindex] = nums[index];
-		combination(index + 1, sindex + 1);
-		combination(index + 1, sindex);
+		// 재귀 조건
+		select[sidx] = idx + 1;
+		combination(idx + 1, sidx + 1);
+		combination(idx + 1, sidx);
 	}
-	
 }
