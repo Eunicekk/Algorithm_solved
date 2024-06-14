34292364a962dfbd4201dfb4af98a9590eb4ec52
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2024-06-14 12:35:53 +0900

    [Silver III] Title: N과 M (1), Time: 348 ms, Memory: 36508 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,14 +3,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// N과 M(1)
 public class Main {
 	
 	static int N;
 	static int M;
-	static boolean[] visited;
-	static int[] numbers;
+	static boolean[] visit;
 	static int[] select;
-	static StringBuilder sb;
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,38 +18,31 @@ public class Main {
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		visited = new boolean[N];
-		numbers = new int[N];
+		visit = new boolean[N];
 		select = new int[M];
-		sb = new StringBuilder();
 		
-		for (int i = 0; i < N; i++) {
-			numbers[i] = i + 1;
-		}
-		
-		premutation(0);
-		System.out.println(sb);
+		permutation(0);
+		System.out.print(sb);
 	}
 	
-	static void premutation(int idx) {
+	static void permutation(int idx) {
 		// 기저 조건
 		if (idx == M) {
-			for (int i = 0; i < M - 1; i++) {
+			for (int i = 0; i < M-1; i++) {
 				sb.append(select[i] + " ");
 			}
-			sb.append(select[M - 1] + "\n");
+			sb.append(select[M-1] + "\n");
 			return;
 		}
 		
 		// 재귀 조건
 		for (int i = 0; i < N; i++) {
-			if (!visited[i]) {
-				visited[i] = true;
-				select[idx] = numbers[i];
-				premutation(idx + 1);
-				visited[i] = false;
+			if (!visit[i]) {
+				visit[i] = true;
+				select[idx] = i + 1;
+				permutation(idx + 1);
+				visit[i] = false;
 			}
 		}
 	}
-	
 }
