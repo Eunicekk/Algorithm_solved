e02e27ea6eb694fcefe4038cd723b3cff515640a
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2024-07-19 23:02:12 +0900

    [240719] 숨어있는 숫자의 덧셈

---
+++ JavaScript/[240719] 숨어있는 숫자의 덧셈.js
@@ -0,0 +1,20 @@
+function solution(my_string) {
+    var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var array = my_string.split("");
+    var answer = 0;
+    
+    for (const item of array) {
+        for (const num of number) {
+            if (item == num) answer += Number(item);
+        }
+    }
+    
+    return answer;
+}
+
+// 테스트 1 〉	통과 (0.17ms, 33.3MB)
+// 테스트 2 〉	통과 (0.21ms, 33.5MB)
+// 테스트 3 〉	통과 (0.23ms, 33.3MB)
+// 테스트 4 〉	통과 (0.22ms, 33.5MB)
+// 테스트 5 〉	통과 (0.16ms, 33.5MB)
+// 테스트 6 〉	통과 (0.16ms, 33.4MB)
