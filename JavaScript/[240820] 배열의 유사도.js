ae940cb327786d72dc694d4bc697e4f33b9d6954
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2024-08-20 13:36:13 +0900

    [240820] 배열의 유사도

---
+++ JavaScript/[240820] 배열의 유사도.js
@@ -0,0 +1,20 @@
+function solution(s1, s2) {
+    var answer = 0;
+    
+    for (const item1 of s1) {
+        for (const item2 of s2) {
+            if (item1 === item2) answer++;
+        }
+    }
+
+    return answer;
+}
+
+// 테스트 1 〉	통과 (0.18ms, 33.4MB)
+// 테스트 2 〉	통과 (0.06ms, 33.5MB)
+// 테스트 3 〉	통과 (0.05ms, 33.6MB)
+// 테스트 4 〉	통과 (0.07ms, 33.5MB)
+// 테스트 5 〉	통과 (0.04ms, 33.4MB)
+// 테스트 6 〉	통과 (0.07ms, 33.5MB)
+// 테스트 7 〉	통과 (0.04ms, 33.5MB)
+// 테스트 8 〉	통과 (0.04ms, 33.4MB)
