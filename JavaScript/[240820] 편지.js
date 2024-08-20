61edbf670110509577e2222941ff7744c28fc9a2
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2024-08-20 09:01:08 +0900

    [240820] 편지

---
+++ JavaScript/[240820] 편지.js
@@ -0,0 +1,8 @@
+function solution(message) {
+    var answer = message.length * 2;
+    return answer;
+}
+
+// 테스트 1 〉	통과 (0.02ms, 33.4MB)
+// 테스트 2 〉	통과 (0.02ms, 33.5MB)
+// 테스트 3 〉	통과 (0.02ms, 33.5MB)
