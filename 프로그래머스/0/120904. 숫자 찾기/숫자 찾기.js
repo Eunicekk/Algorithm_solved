8cdfb024d937fe87c88b699dbaa9f27adf8a0693
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2024-08-22 13:35:30 +0900

    Delete directory

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
+++
@@ -1,9 +0,0 @@
-function solution(num, k) {
-    const array = num.toString().split("");
-    
-    for (let i = 0; i < array.length; i++) {
-        if (Number(array[i]) === k) return i + 1;
-    }
-    
-    return -1;
-}
