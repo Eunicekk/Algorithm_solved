7e3c08cc3cfeab83ba54d82ef5fcd6cb7db70f1a
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2024-08-22 14:08:35 +0900

    [240822] OX퀴즈

---
+++ JavaScript/[240822] OX퀴즈.js
@@ -0,0 +1,35 @@
+function solution(quiz) {
+    var answer = [];
+    
+    for (const formula of quiz) {
+        const array = formula.split(" ");
+        
+        if (array[1] === "+") {
+            if (Number(array[0]) + Number(array[2]) === Number(array[4])) answer.push("O");
+            else answer.push("X");
+        } else {
+            if (Number(array[0]) - Number(array[2]) === Number(array[4])) answer.push("O");
+            else answer.push("X");
+        }
+    }
+    
+    return answer;
+}
+
+// 테스트 1 〉	통과 (0.07ms, 33.5MB)
+// 테스트 2 〉	통과 (0.07ms, 33.4MB)
+// 테스트 3 〉	통과 (0.07ms, 33.6MB)
+// 테스트 4 〉	통과 (0.06ms, 33.3MB)
+// 테스트 5 〉	통과 (0.06ms, 33.4MB)
+// 테스트 6 〉	통과 (0.06ms, 33.6MB)
+// 테스트 7 〉	통과 (0.06ms, 33.4MB)
+// 테스트 8 〉	통과 (0.13ms, 33.4MB)
+// 테스트 9 〉	통과 (0.08ms, 33.5MB)
+// 테스트 10 〉	통과 (0.07ms, 33.6MB)
+// 테스트 11 〉	통과 (0.07ms, 33.7MB)
+// 테스트 12 〉	통과 (0.10ms, 33.6MB)
+// 테스트 13 〉	통과 (0.09ms, 33.4MB)
+// 테스트 14 〉	통과 (0.13ms, 33.4MB)
+// 테스트 15 〉	통과 (0.08ms, 33.6MB)
+// 테스트 16 〉	통과 (0.11ms, 33.6MB)
+// 테스트 17 〉	통과 (0.09ms, 33.4MB)
