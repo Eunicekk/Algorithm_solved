1c5022f72f08ca0a017edbb9191c42ccd3d1e5f9
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2024-08-22 13:34:48 +0900

    [level 0] Title: 숫자 찾기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -0,0 +1,9 @@
+function solution(num, k) {
+    const array = num.toString().split("");
+    
+    for (let i = 0; i < array.length; i++) {
+        if (Number(array[i]) === k) return i + 1;
+    }
+    
+    return -1;
+}
