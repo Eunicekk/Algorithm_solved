50a35569641025f98cc7438c7e69b93254ea81bd
Eunice <108565785+Eunicekk@users.noreply.github.com>
2024-12-27 22:16:45 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.11 ms, Memory: 90.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        List<Integer> answer = new ArrayList<>();
+        String[] todayArray = today.split("\\.");
+        Map<String, Integer> map = new HashMap<>();
+        int length = privacies.length;
+        
+        for (String term : terms) {
+            String[] temp = term.split(" ");
+            map.put(temp[0], Integer.parseInt(temp[1]));
+        }
+        
+        for (int i = 0; i < length; i++) {
+            String[] privacy = privacies[i].split(" ");
+            String[] date = privacy[0].split("\\.");
+            int year = Integer.parseInt(date[0]);
+            int month = Integer.parseInt(date[1]);
+            int day = Integer.parseInt(date[2]);
+            
+            int addYear = map.get(privacy[1]) / 12;
+            int addMonth = map.get(privacy[1]) % 12;
+            
+            int[] deadline = new int[3];
+            deadline[0] = year + addYear;
+            deadline[1] = month + addMonth;
+            deadline[2] = day;
+            
+            if (deadline[1] > 12) {
+                deadline[0] += deadline[1] / 12;
+                deadline[1] = deadline[1] % 12;
+            }
+            
+            int todayYear = Integer.parseInt(todayArray[0]);
+            int todayMonth = Integer.parseInt(todayArray[1]);
+            int todayDay = Integer.parseInt(todayArray[2]);
+
+            if (deadline[0] < todayYear || 
+                (deadline[0] == todayYear && deadline[1] < todayMonth) || 
+                (deadline[0] == todayYear && deadline[1] == todayMonth && deadline[2] <= todayDay)) {
+                answer.add(i + 1);
+            }
+        }
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
