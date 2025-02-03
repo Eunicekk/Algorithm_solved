30b5ab1b3c489c0fe84b164835e0f654da7b4e8f
Eunice <108565785+Eunicekk@users.noreply.github.com>
2025-02-04 00:01:41 +0900

    [Silver IV] Title: 수 찾기, Time: 608 ms, Memory: 45108 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,31 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-// 수 찾기
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] array = new int[N];
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			array[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		Arrays.sort(arr);
+		Arrays.sort(array);
 		
 		int M = Integer.parseInt(br.readLine());
+		int[] numbers = new int[M];
 		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
 		
 		for (int i = 0; i < M; i++) {
-			if (binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0) {
+			if (Arrays.binarySearch(array, numbers[i]) >= 0) {
 				sb.append(1 + "\n");
 			} else {
 				sb.append(0 + "\n");
@@ -33,25 +33,6 @@ public class Main {
 		}
 		
 		System.out.print(sb);
-	}
-	
-	static int binarySearch (int[] arr, int key) {
-		int left = 0; // 왼쪽 인덱스
-		int right = arr.length - 1; // 오른쪽 인덱스
-		
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			
-			if (key < arr[mid]) {
-				right = mid - 1;
-			} else if (key > arr[mid]) {
-				left = mid + 1;
-			} else {
-				return mid;
-			}
-		}
-		
-		return -1;
 	}
 	
 }
