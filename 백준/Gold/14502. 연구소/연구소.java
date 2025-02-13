91364e5281f8e85364f7a8a5187fa25ef2d03e85
Eunice <108565785+Eunicekk@users.noreply.github.com>
2025-02-13 11:45:49 +0900

    [Gold IV] Title: 연구소, Time: 364 ms, Memory: 148516 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,139 +1,93 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-// 연구실
 public class Main {
+
+	static int N, M;
+	static int[][] section;
+	static List<Point> list = new ArrayList<>();
+	static int max;
 	
-	static int N;
-	static int M;
-	static int[][] lab;
-	static int[][] temp; // lab을 deep copy할 임시 배열
-	static boolean[][] visit;
-	static int max = Integer.MIN_VALUE;
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
 	
-	static List<Position> list = new ArrayList<>(); // 벽을 둘 수 있는 공간을 담는 리스트
-	static Position[] select = new Position[3];
-	
-	static int[] dr = {-1, 1, 0, 0}; // 상 하 좌 우
-	static int[] dc = {0, 0, -1 ,1};
-	
-	static class Position {
+	static class Point {
 		int r;
 		int c;
 		
-		public Position(int r, int c) {
+		public Point(int r, int c) {
 			this.r = r;
 			this.c = c;
-		}
-
-		@Override
-		public String toString() {
-			return "Position [r=" + r + ", c=" + c + "]";
 		}
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		lab = new int[N][M];
-		temp = new int[N][M];
-		visit = new boolean[N][M];
+		section = new int[N][M];
 		
-		for (int i = 0; i < N; i++) {
+		for (int r = 0; r < N; r++) {
 			st = new StringTokenizer(br.readLine());
-			
-			for (int j = 0; j < M; j++) {
-				lab[i][j] = Integer.parseInt(st.nextToken());
+			for (int c = 0; c < M; c++) {
+				section[r][c] = Integer.parseInt(st.nextToken());
 				
-				if (lab[i][j] == 0) {
-					list.add(new Position(i, j));
+				if (section[r][c] == 0) {
+					list.add(new Point(r, c));
 				}
 			}
 		}
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				temp[i][j] = lab[i][j];
-			}
-		}
-		
-		comb(0, 0);
-		
+
+		combination(0, 0);
 		System.out.println(max);
 	}
 	
-	static void comb (int idx, int sidx) {
-		if (sidx == 3) {
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					temp[i][j] = lab[i][j];
-				}
-			}
-			
-			visit = new boolean[N][M];
-			bfs(select);
+	static void combination(int depth, int start) {
+		if (depth == 3) {
+			bfs();
 			return;
 		}
 		
-		if (idx == list.size()) {
-			return;
+		for (int i = start; i < list.size(); i++) {
+			Point point = list.get(i);
+			section[point.r][point.c] = 1;
+			combination(depth + 1, i + 1);
+			section[point.r][point.c] = 0;
 		}
-		
-		select[sidx] = list.get(idx);
-		comb(idx + 1, sidx + 1);
-		comb(idx + 1, sidx);
 	}
 	
-	static void bfs (Position[] select) {
-		int count = 0; // 안전영역 개수 세기
-		
-		for (int i = 0; i < 3; i++) {
-			temp[select[i].r][select[i].c] = 1;
-		}
-		
-		Queue<Position> queue = new LinkedList<>();
+	static void bfs() {
+		Queue<Point> queue = new LinkedList<>();
+		int[][] temp = new int[N][M];
 		
 		for (int i = 0; i < N; i++) {
+			temp[i] = section[i].clone();
 			for (int j = 0; j < M; j++) {
-				
-				if (temp[i][j] == 2 && visit[i][j] == false) {
-					queue.offer(new Position(i, j));
-					visit[i][j] = true;
-					
-					while (!queue.isEmpty()) {
-						Position pos = queue.poll();
-						
-						for (int d = 0; d < 4; d++) {
-							int nr = pos.r + dr[d];
-							int nc = pos.c + dc[d];
-							
-							if (nr >= 0 && nc >= 0 && nr < N && nc < M && !visit[nr][nc] && temp[nr][nc] == 0) {
-								temp[nr][nc] = 2;
-								visit[nr][nc] = true;
-								queue.offer(new Position(nr, nc));
-							}
-						}
-					}
+				if (temp[i][j] == 2) {
+					queue.add(new Point(i, j));
 				}
-				
 			}
 		}
 		
+		while (!queue.isEmpty()) {
+			Point point = queue.poll();
+			
+			for (int d = 0; d < 4; d++) {
+				int nr = point.r + dr[d];
+				int nc = point.c + dc[d];
+				
+				if (nr >= 0 && nc >= 0 && nr < N && nc < M && temp[nr][nc] == 0) {
+					temp[nr][nc] = 2;
+					queue.add(new Point(nr, nc));
+				}
+			}
+		}
+		
+		int count = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (temp[i][j] == 0) {
-					count++;
-				}
+				if (temp[i][j] == 0) count++;
 			}
 		}
 		
