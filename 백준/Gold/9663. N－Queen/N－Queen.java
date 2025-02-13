a54fc73e6b25784ba90de5cce829494c1182d83b
Eunice <108565785+Eunicekk@users.noreply.github.com>
2025-02-13 12:27:56 +0900

    [Gold IV] Title: N-Queen, Time: 2312 ms, Memory: 14848 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,52 +1,36 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.*;
+import java.io.*;
 
-// N-Queen
 public class Main {
-	
-	static int[] position;
+
+	static int N;
 	static int count;
+	static boolean[] visit1, visit2, visit3;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		N = Integer.parseInt(br.readLine());
+		visit1 = new boolean[N];
+		visit2 = new boolean[N*2];
+		visit3 = new boolean[N*2];
 		
-		int N = Integer.parseInt(br.readLine()); // 체스판 크기
-		position = new int[N]; // 인덱스 : row, 값 : col
-		count = 0;
-		
-		queen(0, N);
-		
-		bw.write(count + "");
-		bw.close();
+		backtrack(0);
+		System.out.println(count);
 	}
 	
-	static boolean check(int row, int col) {
-		for (int i = 0; i < row; i++) {
-			if (position[i] == col || Math.abs(i - row) == Math.abs(position[i] - col)) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
-	
-	static void queen(int row, int n) {
-		// 기저 조건
-		if (row == n) {
+	static void backtrack(int r) {
+		if (r == N) {
 			count++;
 			return;
 		}
 		
-		// 재귀 조건
-		for (int i = 0; i < n; i++) {
-			if (check(row, i)) {
-				position[row] = i;
-				queen(row + 1, n);
-			}
+		for (int c = 0; c < N; c++) {
+			if (visit1[c] || visit2[r + c] || visit3[r-c+N]) continue;
+			
+			visit1[c] = visit2[r + c] = visit3[r-c+N] = true;
+			backtrack(r + 1);
+			visit1[c] = visit2[r + c] = visit3[r-c+N] = false;
 		}
 	}
+	
 }
