14c0d198b9b1d784db7b9f8165c4ece4176b1a97
Eunice <108565785+Eunicekk@users.noreply.github.com>
2025-02-19 13:22:00 +0900

    [Gold V] Title: 로봇 청소기, Time: 112 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,75 +1,91 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	
-	static int N;
-	static int M;
+
+	static int N, M;
 	static int[][] room;
-	static int count = 1;
-	static int direction;
+	static int count;
 	
-	static int[] dr = {-1, 0, 1, 0}; // 북 동 남 서
+	static int[] dr = {-1, 0, 1, 0};
 	static int[] dc = {0, 1, 0, -1};
 	
-	static class Robot {
+	static class Robot{
 		int r;
 		int c;
-		int d;
+		int di;
 		
-		public Robot(int r, int c, int d) {
+		public Robot (int r, int c, int di) {
 			this.r = r;
 			this.c = c;
-			this.d = d;
+			this.di = di;
 		}
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		room = new int[N][M];
 		
 		st = new StringTokenizer(br.readLine());
-		Robot robot = new Robot(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+		int robot_r = Integer.parseInt(st.nextToken());
+		int robot_c = Integer.parseInt(st.nextToken());
+		int robot_di = Integer.parseInt(st.nextToken());
 		
-		for (int i = 0; i < N; i++) {
+		for (int r = 0; r < N; r++) {
 			st = new StringTokenizer(br.readLine());
-			
-			for (int j = 0; j < M; j++) {
-				room[i][j] = Integer.parseInt(st.nextToken());
+			for (int c = 0; c < M; c++) {
+				room[r][c] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		dfs(robot.r, robot.c, robot.d);
+		bfs(robot_r, robot_c, robot_di);
+		
 		System.out.println(count);
 	}
 	
-	static void dfs (int r, int c, int direction) {
-		room[r][c] = -1;
+	static void bfs(int r, int c, int di) {
+		Queue<Robot> queue = new LinkedList<>();
+		queue.offer(new Robot(r, c, di));
+		if (room[r][c] == 0) {
+			room[r][c] = 2;
+			count++;
+		}
+
+		while (!queue.isEmpty()) {
+			Robot robot = queue.poll();
+			boolean move = false;
+			int nr, nc;
 			
-		for (int i = 0; i < 4; i++) {
-			direction = (direction + 3) % 4;
-			int nr = r + dr[direction];
-			int nc = c + dc[direction];
-			
-			if (nr >= 0 && nc >= 0 && nr < N && nc < M && room[nr][nc] == 0) {
-				count++;
-				dfs(nr, nc, direction);
-				return;
+			for (int d = 0; d < 4; d++) {
+				int index = (robot.di + 3 - d) % 4;
+				if (index < 0) index += 4;
+				
+				nr = robot.r + dr[index];
+				nc = robot.c + dc[index];
+				
+				if (nr >= 0 && nc >= 0 && nr < N && nc < M && room[nr][nc] == 0) {
+					queue.offer(new Robot(nr, nc, index));
+					room[nr][nc] = 2;
+					count++;
+					move = true;
+					break;
+				}
+			}
+
+			if (!move) {
+				nr = robot.r - dr[robot.di];
+				nc = robot.c - dc[robot.di];
+				
+				if (nr < 0 || nc < 0 || nr >= N || nc >= M || room[nr][nc] == 1) {
+					return;
+				}
+				
+				queue.offer(new Robot(nr, nc, robot.di));
 			}
 		}
-			
-		int back = (direction + 2) % 4;
-		int wr = r + dr[back];
-		int wc = c + dc[back];
-		
-		if (wr >= 0 && wc >= 0 && wr < N && wc < M && room[wr][wc] != 1) {
-			dfs(wr, wc, direction);
-		}
 	}
+	
 }
