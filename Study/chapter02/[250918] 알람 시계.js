fca53584e9f51d409967c309e858d7b44759e7a5
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2025-09-18 10:54:47 +0900

    [250918] 알람 시계

---
+++ Study/chapter02/[250918] 알람 시계.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().split(" ");
+
+const H = Number(input[0]);
+const M = Number(input[1]);
+
+if (M >= 45) {
+  console.log(H, M - 45);
+} else {
+  if (H === 0) {
+    console.log(23, M + 15);
+  } else {
+    console.log(H - 1, M + 15);
+  }
+}
