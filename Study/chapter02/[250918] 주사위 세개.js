2bd76558b5e155ef22aba78f0ff2d81bfb02f048
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2025-09-18 16:41:07 +0900

    [250918] 주사위 세개

---
+++ Study/chapter02/[250918] 주사위 세개.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().split(" ");
+
+const a = Number(input[0]);
+const b = Number(input[1]);
+const c = Number(input[2]);
+
+if (a === b && b === c) {
+  console.log(10000 + 1000 + a);
+} else if (a === b) {
+  console.log(1000 + 100 * a);
+} else if (b === c) {
+  console.log(1000 + 100 * b);
+} else if (c === a) {
+  console.log(1000 + 100 * c);
+} else {
+  const max = [a, b, c].reduce((n1, n2) => Math.max(n1, n2));
+  console.log(100 * max);
+}
