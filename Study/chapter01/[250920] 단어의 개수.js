d604a735fa21f9bcb4d9ea33ecb4090ce1af3bed
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2025-09-20 10:40:09 +0900

    [250920] 단어의 개수

---
+++ Study/chapter01/[250920] 단어의 개수.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+if (input == "") {
+  console.log(0);
+} else {
+  console.log(input.length);
+}
